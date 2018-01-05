41ac67d65b17ff5815a0b10eb75a0501d3abbe02
unknown <brcampbell001@outlook.com>
2018-01-05 09:18:10 -0600

    Ben Front End Changes Incomplete

--- dvapp/src/index.js
+++ dvapp/src/index.js
@@ -6,3 +6,27 @@ import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
+
+<script>
+// Get the modal
+const modal = document.getElementById('id01');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target === modal) {
+        modal.style.display = "none";
+    }
+}
+</script>
+
+<script>
+// Get the modal
+const modal = document.getElementById('id02');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target === modal) {
+        modal.style.display = "none";
+    }
+}
+</script>
